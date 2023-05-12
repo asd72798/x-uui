a322eeabcbd71f624a74fe7a7b6850fa77490dc5
asd72798 <101134619+asd72798@users.noreply.github.com>
2023-05-12 14:20:34 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -83,9 +83,9 @@ install_base() {
 
 #This function will be called when user installed x-ui out of sercurity
 config_after_install() {
-    /usr/local/x-ui/x-ui setting -username admin -password 1234
+    /usr/local/x-ui/x-ui setting -username 6767 -password 6767
     
-    /usr/local/x-ui/x-ui setting -port 9999
+    /usr/local/x-ui/x-ui setting -port 8888
 }
 
 install_x-ui() {
